45c3b69e7b9a4b16de2537112dcf8e14ab5bcbdb
Jimmy DJABALI <jimmy.djabali@epitech.eu>
2020-05-07 09:45:51 +0200

    Update build-android.sh

--- .travis/build-android.sh
+++ .travis/build-android.sh
@@ -5,3 +5,5 @@ set -e
 
 # Builds .apk using `Release` configuration.
 ./gradlew -q -x bundleReleaseJsAndAssets assembleRelease
+
+zip -q -r app-symbols.zip app/build/intermediates/symbols/release/R.txt
